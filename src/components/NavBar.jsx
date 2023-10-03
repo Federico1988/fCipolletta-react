3c41367bd42f7a47e3cafb9ba2aace3652162ea5
Fede C <federico.cipolletta@diestroweb.com>
2023-10-03 19:04:58 -0300

    Test estilos y markdown

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,50 @@
 import React from 'react'
 import LogoImg from '../img/logo.png'
 import CartImg from '../img/cart.png'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Flex,
+  Box,
+  Spacer
+} from '@chakra-ui/react'
+
 const NavBar = () => {
 
   return (
-    <nav>
-        <div>
-            <img className='logo' src={LogoImg} alt="brand logo" />
-            <>DropDown</>
-            <img className='logo' src={CartImg} alt="cart" />
-        </div>
-    </nav>
+
+    <div>
+
+      <Flex>
+        <Box p='4' bg='red.400'>
+          <p>Marca</p>
+        </Box>
+        <Spacer />
+        <Box>
+          <Menu>
+            <MenuButton>
+              Categorias
+            </MenuButton>
+            <MenuList>
+              <MenuItem>
+                Opcion 1
+              </MenuItem>
+              <MenuItem>
+                Opcion 2
+              </MenuItem>
+
+            </MenuList>
+          </Menu>
+        </Box>
+        <Spacer />
+        <Box p='4' bg='green.400'>
+
+        </Box>
+      </Flex>
+    </div>
+
   )
 }
 
