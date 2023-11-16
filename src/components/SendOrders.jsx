f2d4ac7dd76cf83692fbae1235878124ea8560ef
Fede C <federico.cipolletta@diestroweb.com>
2023-11-16 14:18:05 -0300

    Agrego formulario de contacto

--- src/components/SendOrders.jsx
+++ src/components/SendOrders.jsx
@@ -15,15 +15,73 @@ const SendOrders = () => {
         email
     }
     const ordersCollection = collection(db, "Mi orden");
-    return (
-        <div>
-            <h1>Formulario de Compra</h1>
-            <form action="" onSubmit={handleSubmit}>
-                <input type="text" placeholder='Ingrese el Nombre' onChange={(e) => setNombre(e.target.value)} />
-                <input type="email" placeholder='Ingrese el email' onChange={(e) => setEmail(e.target.value)} />
-                <button type='sumbit'>Enviar</button>
-            </form>
-        </div>
+    return (<div style={{ textAlign: 'center', margin: '50px' }}>
+        <h1 style={{ marginBottom: '20px' }}>Formulario de Contacto</h1>
+        <form
+            onSubmit={handleSubmit}
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+        >
+            <div style={{ marginBottom: '15px', width: '300px' }}>
+                <label htmlFor="nombre" style={{ marginBottom: '5px', display: 'block' }}>
+                    Nombre:
+                </label>
+                <input
+                    type="text"
+                    id="nombre"
+                    placeholder="Ingrese el Nombre"
+                    value={nombre}
+                    onChange={(e) => setNombre(e.target.value)}
+                    style={{
+                        width: '100%',
+                        padding: '10px',
+                        border: '1px solid #ccc',
+                        borderRadius: '5px',
+                        boxSizing: 'border-box',
+                    }}
+                    required
+                />
+            </div>
+            <div style={{ marginBottom: '15px', width: '300px' }}>
+                <label htmlFor="email" style={{ marginBottom: '5px', display: 'block' }}>
+                    Email:
+                </label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Ingrese el email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    style={{
+                        width: '100%',
+                        padding: '10px',
+                        border: '1px solid #ccc',
+                        borderRadius: '5px',
+                        boxSizing: 'border-box',
+                    }}
+                    required
+                />
+            </div>
+            <div>
+                <button
+                    type="submit"
+                    style={{
+                        padding: '10px 20px',
+                        background: '#4CAF50',
+                        color: 'white',
+                        border: 'none',
+                        borderRadius: '5px',
+                        cursor: 'pointer',
+                    }}
+                >
+                    Enviar
+                </button>
+            </div>
+        </form>
+    </div>
     )
 }
 
